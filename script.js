2a11c95057405ee0b2bc4c91756d7041ad576554
Sharon Malodol @ Jamilon <101910692+ShronMJ@users.noreply.github.com>
2023-10-26 02:41:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,52 +7,58 @@ const form = document.getElementById("form");
 const search = document.getElementById("query");
 
 returnMovies(APILINK);
-
-function returnMovies(url){
-    fetch(url).then(res => res.json()) //fetch
-    .then(function(data){
-        console.log(data.results);
-        data.results.forEach(element => {
-            const div_card = document.createElement('div');
-            div_card.setAttribute('class','card');
-
-            const div_row = document.createElement('div');
-            div_row.setAttribute('class','row');
-
-            const div_column = document.createElement('div');
-            div_column.setAttribute('class','column');
-
-            const image = document.createElement('img');
-            image.setAttribute('class','thumbnail');
-            image.setAttribute('id','image');
-
-            const title = document.createElement('h3');
-            title.setAttribute('id','title');
-
-
-            const center = document.createElement('center');
-
-            title.innerHTML = `${element.title}`
-            image.src = IMG_PATH + element.poster_path;
-
-            center.appendChild(image);
-            div_card.appendChild(center);
-            div_card.appendChild(title);
-            div_column.appendChile(div_card);
-            div_row.appendChild(div_column);
-
-            main.appendChild(div_row);
-        });
-    });
-
+findMovies();
+function findMovies(){
     form.addEventListener("submit", (e)=>{
         e.preventDefault();
         main.innerHTML=''
         const searchItem = search.value;
-        
-        if(searchItem){
-            returnMovies(SEARCH_API+searchItem);
-            search.value ='';
-        }
+        console.log(searchItem);
+        returnMovies(SEARCH_API+searchItem);
+        search.value ='';
     })
+}
+
+function returnMovies(url){
+    fetch(url).then(res => res.json()) //fetch
+    .then(function(data){
+        let totalDisplay = data.results.length;
+        totalDisplay>0 ? displaySearch(data) : returnMovies(APILINK);
+        
+        console.log(totalDisplay);  
+    });
 };
+
+function displaySearch(data){
+    data.results.forEach(element => {
+        const div_card = document.createElement('div');
+        div_card.setAttribute('class','card');
+
+        const div_row = document.createElement('div');
+        div_row.setAttribute('class','row');
+
+        const div_column = document.createElement('div');
+        div_column.setAttribute('class','column');
+
+        const image = document.createElement('img');
+        image.setAttribute('class','thumbnail');
+        image.setAttribute('id','image');
+
+        const title = document.createElement('h3');
+        title.setAttribute('id','title');
+
+
+        const center = document.createElement('center');
+
+        title.innerHTML = `${element.title}`;
+        image.src = IMG_PATH + element.poster_path;
+
+        center.appendChild(image);
+        div_card.appendChild(center);
+        div_card.appendChild(title);
+        div_column.appendChild(div_card);
+        div_row.appendChild(div_column);
+
+        main.appendChild(div_row);
+    });
+}
